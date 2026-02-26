801103f57a8ce378f2416987aed340e313373cb6
Kip <kip.riecken@gmail.com>
2026-02-26 13:13:43 -0500

    change /hello to /health, return json

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/hello")
-def hello():
-    return "Hello World"
+
+@app.route("/health")
+def health():
+    return jsonify({"status": "ok", "message": "Lyrics API is healthy"})
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
